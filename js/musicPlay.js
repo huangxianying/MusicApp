74ffdf16beb1fa8aaf4b6102882794e811e92cc5
huang <524172671@qq.com>
2016-11-30 00:01:50 +0800

    添加了歌曲列表

--- js/musicPlay.js
+++ js/musicPlay.js
@@ -2,7 +2,7 @@
 
     $(document).ready(function(){
         var songList = [];
-        var song = {};
+        // var song = {};
         $.ajax({
             url:'http://tingapi.ting.baidu.com/v1/restserver/ting',
             data:{
@@ -14,49 +14,45 @@
             dataType:'jsonp',
             success:function(res){
                 console.log(res);
-                var list = res.song_list
+                var list = res.song_list;
+                // console.log(list);
                 list.forEach(function(item,idx){
-                    song.title = item.title +'-'+ item.style
-                for(var i=0;i<song.length;i++){
-                    songList.push(song[i])
-                     console.log(songList);      
-                }
+                    // console.log(item.title);
+                    // 创建对象并添加属性
+                    var song = {};
+                    $.ajax({
+                        url:'http://tingapi.ting.baidu.com/v1/restserver/ting',
+                        data:{
+                            method:'baidu.ting.song.play',
+                            songid:item.song_id
+                        },
+                        dataType:'jsonp',
+                        success:function(res){
+                            song.mp3 = res.bitrate.show_link;
+                        }
+                    });
+
+                    song.title = item.title +'-'+ item.style;
+                    songList.push(song);
                 })
-
-
+                musicPlay(songList)
+                console.log(songList);
             }
-        })
-
-
-
+        });
+function musicPlay(attr){
     new jPlayerPlaylist({
         jPlayer: "#jquery_jplayer_1",
         cssSelectorAncestor: "#jp_container_1"
-    }, [
-
-        {
-            title:"歌曲一",
-            mp3:"http://zhejiang.mycodes.net/MP3/7631.mp3",
-            oga:"http://zhejiang.mycodes.net/MP3/7631.mp3"
-        },
-        {
-            title:"歌曲二",
-            mp3:"http://zhejiang.mycodes.net/MP3/7463.mp3",
-            oga:"http://zhejiang.mycodes.net/MP3/7463.mp3"
-        },
-        {
-            title:"歌曲三",
-            mp3:"http://zhejiang.mycodes.net/MP3/7399.mp3",
-            oga:"http://zhejiang.mycodes.net/MP3/7399.mp3"
-        }
-    ], {
-        swfPath: "../../dist/jplayer",
-        supplied: "oga, mp3",
+    },attr, {
+        swfPath: "../../../dist/jplayer",
+        supplied: "mp3",
         wmode: "window",
         useStateClassSkin: true,
         autoBlur: false,
         smoothPlayBar: true,
         keyEnabled: true
     });
+}
+
 });
 })();
